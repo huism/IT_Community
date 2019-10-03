97afd38a51b6d4aa2d82ba3666c2bafd02921113
huism <huism007@gmail.com>
2019-10-03 22:13:43 +0800

    init repo

---
+++ src/main/java/com/huism/community/ItCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.huism.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItCommunityApplication.class, args);
+	}
+
+}
