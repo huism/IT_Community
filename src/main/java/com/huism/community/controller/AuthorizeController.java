9a475c260ef0d16a84318ea5bdfb13e186882318
huism <huism007@gmail.com>
2019-10-05 11:23:44 +0800

    add cookie and keep login status

--- src/main/java/com/huism/community/controller/AuthorizeController.java
+++ src/main/java/com/huism/community/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +34,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request) {
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
@@ -44,14 +45,16 @@ public class AuthorizeController {
         GithubUser  githubUser= githubProvider.getUser(accessToken);
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setName(githubUser.getName());
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            // 每次重新登录都会插入一条相同用户但是token不同的数据
             userMapper.insert(user);
             // 登录成功，写session保存登录状态
-            request.getSession().setAttribute("user", githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             // 登录失败，重定向到首页重新登录
--- src/main/java/com/huism/community/mapper/UserMapper.java
+++ src/main/java/com/huism/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.huism.community.mapper;
 import com.huism.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -10,5 +12,6 @@ public interface UserMapper {
     @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
-
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
